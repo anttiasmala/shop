14451a8a98ffadd7b1185a77e12c2afe57c41234
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-13 11:45:27 +0300

    Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import Image from 'next/image';
-import { Geist, Geist_Mono } from 'next/font/google';
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <main className="h-screen w-full bg-gray-500">
+      <div className="flex justify-center">
+        <Image
+          src={'/logo.jpg'}
+          alt="background"
+          width={1920}
+          height={1080}
+          className=""
+        />
+      </div>
+      <div className="w-full flex justify-center mt-10">
+        <button className="border-1 text-white border-black rounded-lg text-4xl ">
+          Maistuisiko kahvi?
+        </button>
+      </div>
+    </main>
   );
 }
