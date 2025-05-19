3328d176f7ec3cdfd2c4061883fad0707e0d8142
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-19 11:45:53 +0300

    Move files and create a new one

---
+++ pages/shop/index.tsx
@@ -0,0 +1,9 @@
+export default function Shop() {
+  return (
+    <main className="h-screen w-full bg-white">
+      <div className="w-full">
+        <div className="flex justify-center">a</div>
+      </div>
+    </main>
+  );
+}
