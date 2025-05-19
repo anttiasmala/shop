f7242475d9b0c05ede5280d79231a1e26319ba42
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-19 12:00:33 +0300

    Add some icons

---
+++ icons/store_bag.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from 'react';
+const SvgStoreBag = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="storeBag_svg__sc-cfxfcM storeBag_svg__chgrbA"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M17.25 6.25H15.5c0-1.934-1.566-3.5-3.5-3.5s-3.5 1.566-3.5 3.5H6.75C5.788 6.25 5 7.038 5 8v10.5c0 .963.788 1.75 1.75 1.75h10.5c.963 0 1.75-.787 1.75-1.75V8c0-.962-.787-1.75-1.75-1.75M12 4.5c.963 0 1.75.788 1.75 1.75h-3.5c0-.962.788-1.75 1.75-1.75m5.25 14H6.75V8H8.5v1.75c0 .481.394.875.875.875s.875-.394.875-.875V8h3.5v1.75c0 .481.394.875.875.875s.875-.394.875-.875V8h1.75z" />
+  </svg>
+);
+export default SvgStoreBag;
