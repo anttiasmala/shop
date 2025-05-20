51ed5b27a630ad7f15b728cecca3f34adb809e40
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-20 10:20:07 +0300

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="fixed bottom-0 h-8 w-full">
+      <div className="flex justify-center">
+        <p className="text-xs text-gray-600">
+          © MINIMONEY Shop. All rights reserverd.
+        </p>
+      </div>
+    </footer>
+  );
+}
