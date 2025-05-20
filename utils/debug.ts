35eff9f183622bfeb9e4083bc973fb27c9edc4ad
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-20 13:37:48 +0300

    Change price and title in one of the products

--- utils/debug.ts
+++ utils/debug.ts
@@ -21,8 +21,8 @@ export const arrayOfProducts: Product[] = [
   },
   {
     id: 4,
-    price: '$199.99',
-    title: 'Samsung T7 SSD 1TB',
+    price: '$1999.99',
+    title: 'Samsung T777 SSD 1TB',
     image: '/Samsung_T5_SSD.png',
   },
 ];
