43988d303f76e19cd77eb94e08e38ee559f5f86c
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-20 13:13:08 +0300

    Add ToastContainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Bounce, ToastContainer } from 'react-toastify';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,20 @@ export default function App({ Component, pageProps }: AppProps) {
           content="initial-scale=1.0, interactive-widget=resizes-visual"
         />
       </Head>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+        limit={1}
+      />
       <Component {...pageProps} />
     </div>
   );
