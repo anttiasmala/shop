a6b0846e9ceb4232a13e0e2cd5f063d40257c5e0
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-20 13:19:50 +0300

    Add some data to debug.ts

---
+++ utils/debug.ts
@@ -0,0 +1,22 @@
+import { Product } from '~/shared/types';
+
+export const arrayOfProducts: Product[] = [
+  {
+    id: 1,
+    price: '$169.99',
+    title: 'Samsung T5 SSD 1TB',
+    picture: '/Samsung_T5_SSD.png',
+  },
+  {
+    id: 2,
+    price: '$179.99',
+    title: 'Samsung T6 SSD 1TB',
+    picture: '/Samsung_T5_SSD.png',
+  },
+  {
+    id: 3,
+    price: '$199.99',
+    title: 'Samsung T7 SSD 1TB',
+    picture: '/Samsung_T5_SSD.png',
+  },
+];
