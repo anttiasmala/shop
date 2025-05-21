0ea875c91365eb9a8b84429014c7d4273d570776
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-21 10:22:57 +0300

    Add product API

---
+++ pages/api/product/[id].ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '~/prisma';
+
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  console.log('ASD');
+  if (req.method !== 'GET') {
+    return res.status(405).send('GET request is the only allowed method');
+  }
+  const queryId = req.query.id;
+  if (queryId === undefined || typeof queryId !== 'string') {
+    return res.status(400).send('ID is mandatory in query');
+  }
+  const numberQueryId = Number(queryId);
+  if (Number.isNaN(numberQueryId)) {
+    return res.status(400).send('Invalid ID given');
+  }
+
+  const product = await prisma.product.findUnique({
+    where: { id: numberQueryId },
+  });
+
+  res.status(200).json(product);
+}
