28973d723713589affb5ef12da6a1889fa898290
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-21 10:22:34 +0300

    Add QueryClient and QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,31 +2,36 @@ import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Bounce, ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Head>
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, interactive-widget=resizes-visual"
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, interactive-widget=resizes-visual"
+          />
+        </Head>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+          limit={1}
         />
-      </Head>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-        limit={1}
-      />
-      <Component {...pageProps} />
-    </div>
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
   );
 }
