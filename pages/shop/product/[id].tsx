2794dc8197afa0b17332900373e9ef7d3c132abe
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-21 10:23:59 +0300

    Some testing code in shop/product/id

---
+++ pages/shop/product/[id].tsx
@@ -0,0 +1,50 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+export default function HandleProduct() {
+  const [productId, setProductId] = useState('');
+
+  const router = useRouter();
+
+  const { refetch } = useQuery({
+    queryKey: ['product'],
+    queryFn: async () => {
+      return (await axios.get(`/api/product/${productId}`)).data;
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    retry: false,
+    enabled: false,
+  });
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    console.log(router.query.id);
+    if (router.query.id && typeof router.query.id === 'string') {
+      setProductId(router.query.id);
+    }
+  }, [router.isReady]);
+
+  useEffect(() => {
+    if (productId) refetch();
+  }, [productId]);
+
+  return (
+    <div>
+      <p>Hello: {productId}</p>
+      <button
+        onClick={() => {
+          try {
+            refetch();
+          } catch (e) {
+            console.error(e);
+          }
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+}
