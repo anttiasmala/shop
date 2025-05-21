41a70c3f0c62e3dc6b31d582a04af7d6a9d7f04d
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-21 12:55:42 +0300

    Small changes to apiRequests

--- utils/apiRequests.ts
+++ utils/apiRequests.ts
@@ -1,11 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { Product } from '~/shared/types';
 
-export function getProducts() {
-  useQuery({
+export function useGetProducts() {
+  return useQuery({
     queryKey: ['products'],
     queryFn: async () => {
-      return (await axios.get(`/api/products`)).data;
+      return (await axios.get(`/api/products`)).data as Product[];
+    },
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    retry: false,
+  });
+}
+
+export function useGetProduct(id: string) {
+  return useQuery({
+    queryKey: ['product'],
+    queryFn: async () => {
+      return (await axios.get(`/api/products/${id}`)).data;
     },
     refetchOnWindowFocus: false,
     refetchOnMount: false,
