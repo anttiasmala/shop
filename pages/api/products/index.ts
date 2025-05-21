4d4dd499e13672ed710486ecdd6e5f29356f96af
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-21 11:06:23 +0300

    Add small test code into Handler

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,6 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { handleError } from '~/backend/handleError';
+import prisma from '~/prisma';
 
-export default function Handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).end();
-  return;
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).send('GET request is the only allowed method');
+    }
+
+    const products = await prisma.product.findMany({
+      omit: {
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    res.status(200).json(products);
+    return;
+  } catch (e) {
+    return handleError(res, e);
+  }
 }
