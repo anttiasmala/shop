c1a5a432524e07e337ed168bfb011db0a0404f2b
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-21 15:38:39 +0300

    Add arrow-left.svg and credits for it

---
+++ icons/arrow_left.tsx
@@ -0,0 +1,19 @@
+import type { SVGProps } from 'react';
+const SvgArrowLeft = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={1.5}
+    className="arrow-left_svg__size-6"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+    />
+  </svg>
+);
+export default SvgArrowLeft;
