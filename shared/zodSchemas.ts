c4233fa2e206d8a48829eb2bd47c201362b41d98
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-22 20:12:03 +0300

    Add User and Session schemas from my other project

--- shared/zodSchemas.ts
+++ shared/zodSchemas.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { emailRegex, passwordRegex } from './regexPatterns';
 
 export const productSchema = z.object({
   id: z.number(),
@@ -14,3 +15,113 @@ export const cartSchema = productSchema
     amount: z.number(),
   })
   .array();
+
+// MISC SCHEMAS
+export const uuidSchema = z
+  .string({ message: 'Invalid UUID! It should be given as a string!' })
+  .uuid('UUID pattern was invalid!');
+
+export const dateSchema = z
+  .string()
+  .min(1, 'Date is mandatory!')
+  .datetime({ message: 'Given date is invalid!' })
+  .pipe(z.coerce.date());
+
+export const firstNameSchema = z
+  .string()
+  .min(1, 'Etunimi on pakollinen!')
+  .max(128, 'Etunimi on liian pitkä, maksimipituus on 128 merkkiä');
+
+export const lastNameSchema = z
+  .string()
+  .min(1, 'Sukunimi on pakollinen!')
+  .max(128, 'Sukunimi on liian pitkä, maksimipituus on 128 merkkiä');
+
+export const emailSchema = z
+  .string()
+  .min(1, 'Sähköposti on pakollinen!')
+  .max(128, 'Sähköposti on liian pitkä, maksimipituus on 128 merkkiä')
+  .regex(emailRegex, 'Sähköposti on virheellinen')
+  .transform((value) => value.toLowerCase());
+
+export const passwordSchema = z
+  .string()
+  .min(1, 'Salasana on pakollinen!')
+  .max(128, 'Salasana on liian pitkä, maksimipituus on 128 merkkiä')
+  .regex(
+    passwordRegex,
+    'Salasanan täytyy olla vähintään 8 merkkiä pitkä, maksimissaan 128 merkkiä pitkä, sekä sisältää vähintään yksi iso kirjain, yksi pieni kirjain, yksi numero ja yksi erikoismerkki!',
+  );
+
+// USER SCHEMAS
+
+export const fullUserSchema = z.object({
+  id: z.number().min(1, 'ID is mandatory!'),
+  uuid: uuidSchema,
+  firstName: firstNameSchema,
+  lastName: lastNameSchema,
+  email: emailSchema,
+  password: passwordSchema,
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export const userSchema = fullUserSchema.pick({
+  email: true,
+  firstName: true,
+  lastName: true,
+});
+
+export const getUserSchema = fullUserSchema
+  .pick({
+    uuid: true,
+    createdAt: true,
+    updatedAt: true,
+  })
+  .merge(userSchema);
+
+export const createUserSchema = userSchema.extend({
+  password: passwordSchema,
+});
+
+// SESSION SCHEMAS
+
+export const fullSessionSchema = z.object({
+  uuid: uuidSchema,
+  userUUID: uuidSchema,
+  expiresAt: z.date(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  isLoggedIn: z.boolean(),
+});
+
+export const getSessionSchema = fullSessionSchema.pick({
+  uuid: true,
+  userUUID: true,
+  expiresAt: true,
+  isLoggedIn: true,
+});
+
+export const sessionSchema = getSessionSchema;
+
+export const frontendSessionSchema = getSessionSchema.extend({
+  fresh: z.boolean(),
+});
+
+export const createSessionSchema = getSessionSchema.pick({
+  uuid: true,
+  userUUID: true,
+  expiresAt: true,
+});
+
+export const validSessionResultSchema = z.object({
+  status: z.literal('valid'),
+  databaseSession: sessionSchema,
+  databaseUser: getUserSchema,
+});
+
+export const invalidSessionResultSchema = z.object({
+  status: z.literal('invalid'),
+  databaseSession: z.null(),
+  databaseUser: z.null(),
+});
