1e8127ff72eab8c595ae99883309839b473efd0f
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-22 19:39:00 +0300

    Add a page for cart.tsx

---
+++ pages/shop/cart.tsx
@@ -0,0 +1,13 @@
+import { Footer } from '~/components/Footer';
+import { NavBar } from '~/components/NavBar';
+
+export default function Cart() {
+  return (
+    <main className="h-screen w-full bg-white">
+      <div className="w-full">
+        <NavBar />
+      </div>
+      <Footer />
+    </main>
+  );
+}
