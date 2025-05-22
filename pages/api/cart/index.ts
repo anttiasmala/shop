a1d0461c272d6d9f130ab6fd439203f038c2b578
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-22 20:28:01 +0300

    Add simple functions for handling GET and POST request, they're empty for now

--- pages/api/cart/index.ts
+++ pages/api/cart/index.ts
@@ -1,5 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '~/prisma';
 
 export default function Handler(req: NextApiRequest, res: NextApiResponse) {
+  const requestMethod = req.method;
+  if (requestMethod === 'GET') {
+  }
+
+  if (requestMethod === 'POST') {
+  }
   res.status(200).end();
 }
+
+async function handleGET() {}
+
+function handlePOST() {}
