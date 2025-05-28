a93d9e5451d459f7ea1ee3027eec1d4990efa73e
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-28 13:43:53 +0300

    Add HTML Table, EditModal etc

--- pages/shop/admin/products/index.tsx
+++ pages/shop/admin/products/index.tsx
@@ -1,14 +1,123 @@
+import { Pencil, Trash2 } from 'lucide-react';
 import Link from 'next/link';
+import {
+  HTMLAttributes,
+  ReactNode,
+  TdHTMLAttributes,
+  useEffect,
+  useState,
+} from 'react';
+import { twMerge } from 'tailwind-merge';
+import { Input } from '~/components/Input';
+import { Product } from '~/shared/types';
+import { useGetProducts } from '~/utils/apiRequests';
 
 export default function ProductsIndex() {
+  const [editModalData, setEditModalData] = useState<Product | undefined>();
+  const [deleteModalData, setDeleteModalData] = useState<Product | undefined>();
+
   return (
     <main className="h-screen w-full bg-white">
-      <div className="flex w-full justify-center">
-        <div className="w-full sm:max-w-1/2">
+      <div className="w-full">
+        <div className="flex w-full flex-col items-center">
           <p className="animate-[opacity_1200ms] text-center">Products panel</p>
-          <div className="mt-5 flex w-full justify-center"></div>
+          <div className="mt-5 flex justify-center">
+            <ProductTable />
+
+            {}
+          </div>
         </div>
       </div>
     </main>
+  );
+}
+
+function ProductTable() {
+  const { data: products } = useGetProducts();
+
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
+  return (
+    <table className="block w-56 overflow-auto overflow-y-auto sm:w-96 lg:w-10/12">
+      <thead>
+        <tr className="">
+          <th>Title</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Image</th>
+          <th>Category</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody className="border-4 border-black">
+        {products?.map((_product, _index) => (
+          <tr
+            key={`product_${_index}`}
+            className={` ${_index % 2 == 0 ? 'bg-gray-200' : 'bg-gray-300'}`}
+          >
+            <Td>{_product.title}</Td>
+            <Td>{_product.description}</Td>
+            <Td>{_product.price}</Td>
+            <Td>{_product.image}</Td>
+            <Td>{_product.category}</Td>
+            <Td>
+              <div className="flex">
+                <button
+                  className="m-2 border border-gray-500 p-2 hover:bg-gray-500"
+                  onClick={() => setEditModalData(_product)}
+                >
+                  <Pencil />
+                </button>
+                <button className="m-2 border border-gray-500 p-2 hover:bg-gray-500">
+                  <Trash2 />
+                </button>
+              </div>
+            </Td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function Td({ children, className }: HTMLAttributes<HTMLTableCellElement>) {
+  return (
+    <td className={twMerge(`border border-black p-6`, className)}>
+      {children}
+    </td>
+  );
+}
+
+function EditModal({ product }: { product: Product }) {
+  const {} = product;
+
+  return (
+    <div>
+      <div className="fixed top-0 left-0 z-98 h-full w-full bg-black opacity-80" />
+      <div className="fixed top-[30%] left-[50%] z-99 translate-x-[-50%] translate-y-[-50%]">
+        <div className="flex flex-col">
+          <label className="text-white">Title:</label>
+          <Input />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-white">Description:</label>
+          <Input />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-white">Price:</label>
+          <Input />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-white">Image:</label>
+          <Input />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-white">Category:</label>
+          <Input />
+        </div>
+      </div>
+    </div>
   );
 }
