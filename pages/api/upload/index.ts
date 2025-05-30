4ff334d47b6f7a0342d9ed48f98a9fe2f1414b02
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-05-30 11:07:04 +0300

    Add backend checks for file sent to backend

--- pages/api/upload/index.ts
+++ pages/api/upload/index.ts
@@ -1,9 +1,81 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import formidable from 'formidable';
+import fs from 'fs';
 
-export default function UploadHandler(
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const MIME_TO_FILE = {
+  'image/png': ['.png'],
+  'image/jpeg': ['.jpg', '.jpeg'],
+};
+
+const uploadFolderLocation = './public/images/products';
+
+export default async function UploadHandler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log(req.method);
-  res.status(200).end();
+  try {
+    const form = formidable({
+      uploadDir: uploadFolderLocation,
+      keepExtensions: true,
+      maxFileSize: 5 * 1024 * 1024, // 5 MB
+      filename(name, ext, part, form) {
+        return `${name}${ext}`;
+      },
+
+      filter({ originalFilename, mimetype, name }) {
+        console.log(originalFilename);
+        const allowedMimeTypes = ['image/png', 'image/jpeg'];
+
+        const isValidMimetype = allowedMimeTypes.includes(mimetype || '');
+
+        if (!isValidMimetype) {
+          console.warn(
+            `Rejected file: ${originalFilename} with invalid MIME type: ${mimetype}`,
+          );
+          return false;
+        }
+
+        console.log(MIME_TO_FILE[mimetype as keyof typeof MIME_TO_FILE]);
+        try {
+          const access = fs.accessSync(
+            `${uploadFolderLocation}/${originalFilename}`,
+          );
+
+          if (access === undefined) {
+            console.log('Returning false');
+            return false;
+          }
+        } catch (e) {
+          return true;
+        }
+
+        return false;
+      },
+    });
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        console.error('Error parsing form:', err);
+        return res.status(500).json({ error: 'File upload failed.' });
+      }
+
+      const uploadedFile = files.myFile;
+
+      if (!uploadedFile) {
+        return res.status(400).json({ error: 'No file uploaded.' });
+      }
+
+      console.log('File uploaded successfully:', uploadedFile);
+    });
+
+    res.status(200).end();
+  } catch (e) {
+    console.error(e);
+  }
 }
