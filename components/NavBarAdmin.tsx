0dbb82c79fb566bd8a477dc40be5bb00e73044db
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-01 20:07:28 +0300

    Add NavBarAdmin.tsx

---
+++ components/NavBarAdmin.tsx
@@ -0,0 +1,69 @@
+import Link from 'next/link';
+import { HTMLAttributes, useEffect, useState } from 'react';
+import SvgMenu from '~/icons/menu';
+import SvgStoreBag from '~/icons/store_bag';
+import { twMerge } from 'tailwind-merge';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { GetCart, QueryAndMutationKeys } from '~/shared/types';
+
+export function NavBarAdmin() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="flex h-16 w-full items-center justify-between bg-white shadow-lg">
+        <Link
+          href={'/shop'}
+          className="ml-4 text-lg font-bold sm:ml-[15%] sm:text-xl"
+        >
+          MINIMONEY
+        </Link>
+        <div className="mr-3 flex sm:absolute sm:left-1/2">
+          <button onClick={() => setIsMenuOpen((prevValue) => !prevValue)}>
+            {isMenuOpen ? (
+              <span className="text-3xl font-bold">X</span>
+            ) : (
+              <SvgMenu className="sm:hidden" width={30} height={30} />
+            )}
+          </button>
+          <LinkElement
+            href={'/shop/admin'}
+            className="hidden hover:text-gray-600 sm:inline"
+          >
+            Home Admin Page
+          </LinkElement>
+        </div>
+      </div>
+      <Menu isMenuOpen={isMenuOpen} />
+    </div>
+  );
+}
+
+function Menu({ isMenuOpen }: { isMenuOpen: boolean }) {
+  if (!isMenuOpen) return null;
+  return (
+    <div className="h-24 w-full shadow-lg">
+      <div className="w-full">
+        <LinkElement href={'/shop'} className="block p-3">
+          Home
+        </LinkElement>
+        <LinkElement href={'/shop/products'} className="block p-3 pt-0">
+          Shop
+        </LinkElement>
+      </div>
+    </div>
+  );
+}
+
+function LinkElement({
+  href,
+  className,
+  children,
+}: { href: string } & HTMLAttributes<HTMLDivElement>) {
+  return (
+    <Link className={twMerge('hover:text-gray-600', className)} href={href}>
+      {children}
+    </Link>
+  );
+}
