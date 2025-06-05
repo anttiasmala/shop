3f09dff9ff1940b4ae9ae3ab53ba5e270a7af6e1
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-05 15:27:37 +0300

    Add backend authentication system from another project I am working on

---
+++ backend/auth/auth.ts
@@ -0,0 +1,92 @@
+import type { IncomingMessage, ServerResponse } from 'http';
+import { TimeSpan } from './auth/index';
+import prisma from '~/prisma';
+import type { User, FrontendSession, GetUser } from '~/shared/types';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { HttpError } from '~/backend/HttpError';
+import { AuthAdapter } from './db-adapter';
+import { Auth } from './auth/index';
+
+const prismaAdapter = new AuthAdapter(prisma);
+
+/** 1 hour */
+export const auth = new Auth(prismaAdapter, {
+  sessionExpiresIn: new TimeSpan(1, 'h'),
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+});
+
+/** 30 days | 1 month */
+export const authLong = new Auth(prismaAdapter, {
+  sessionExpiresIn: new TimeSpan(30, 'd'),
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+});
+
+export async function validateRequest(
+  req: IncomingMessage,
+  res: ServerResponse,
+): Promise<
+  { user: GetUser; session: FrontendSession } | { user: null; session: null }
+> {
+  const sessionId = authLong.readSessionCookie(req.headers.cookie ?? '');
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+  const result = await authLong.validateSession(sessionId);
+  if (result.session && result.session.fresh) {
+    res.appendHeader(
+      'Set-Cookie',
+      authLong.createSessionCookie(result.session.uuid).serialize(),
+    );
+  }
+  if (!result.session) {
+    res.appendHeader(
+      'Set-Cookie',
+      authLong.createBlankSessionCookie().serialize(),
+    );
+  }
+
+  return result;
+}
+
+export async function requireLogin(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<{ user: GetUser; session: FrontendSession }> {
+  const userDetails = await validateRequest(req, res);
+  if (
+    !userDetails.session ||
+    !userDetails.user ||
+    !userDetails.session.isLoggedIn
+  ) {
+    throw new HttpError('You are unauthorized!', 401);
+  }
+  return userDetails;
+}
+
+/**
+ * Checks for a valid user and session, **REGARDLESS** of login status
+ *
+ * **ATTENTION**: the user **DOES NOT** need to be logged in for this function to validate request
+ */
+
+export async function checkIfSessionValid(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<{ user: User; session: FrontendSession }> {
+  const userData = await validateRequest(req, res);
+  if (!userData.session || !userData.user) {
+    throw new HttpError('You are unauthorized!', 401);
+  }
+  return userData;
+}
---
+++ backend/auth/db-adapter/index.ts
@@ -0,0 +1,153 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  CreateSession,
+  DatabaseAdapter,
+  Session,
+  GetUserAndSessionResult,
+} from '~/shared/types';
+import { User } from '~/shared/types';
+import { sessionSchema } from '~/shared/zodSchemas';
+
+declare global {
+  var prisma: undefined | PrismaClient; //eslint-disable-line no-var
+}
+
+export class AuthAdapter implements DatabaseAdapter {
+  private prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient | undefined) {
+    this.prisma = prisma ?? new PrismaClient();
+  }
+
+  private async _getUser(userUUID: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { uuid: userUUID },
+    });
+
+    return user ? user : null;
+  }
+
+  async createSession(sessionData: CreateSession): Promise<Session | null> {
+    const { expiresAt, userUUID, uuid } = sessionData;
+    // perhaps isLoggedIn: true here?
+    const session = await this.prisma.session.create({
+      data: { uuid, userUUID, expiresAt, isLoggedIn: true },
+    });
+
+    return session;
+  }
+
+  async deleteSession(sessionUUID: string): Promise<void> {
+    await this.prisma.session.delete({ where: { uuid: sessionUUID } });
+    return;
+  }
+
+  async getSession(sessionUUID: string): Promise<Session | null> {
+    const session = await this.prisma.session.findUnique({
+      where: { uuid: sessionUUID },
+    });
+
+    return session;
+  }
+
+  /** **Potentially a risky function** */
+  async getUserFromSession(sessionUUID: string): Promise<User | null> {
+    const session = await this.prisma.session.findUnique({
+      where: { uuid: sessionUUID },
+      select: { User: { omit: { id: true, password: true } } },
+    });
+
+    if (!session || !session.User) return null;
+
+    return session.User;
+  }
+
+  async getUserAndSessions(
+    userUUID: string,
+  ): Promise<[Session[], User] | null> {
+    const sessions = await this.prisma.session.findMany({
+      where: { userUUID: userUUID },
+    });
+
+    // return null if sessions were not found
+    if (sessions.length <= 0) return null;
+
+    // if sessions were found get the user
+    const user = await this._getUser(userUUID);
+
+    // if user does not exist return null
+    if (!user) return null;
+
+    return [sessions, user];
+  }
+
+  /** **Returns the owner user of the session and the session itself** */
+  async getUserAndSession(
+    sessionUUID: string,
+  ): Promise<GetUserAndSessionResult> {
+    const sessionFromDatabase = await this.prisma.session.findUnique({
+      where: { uuid: sessionUUID },
+      select: {
+        uuid: true,
+        createdAt: true,
+        isLoggedIn: true,
+        updatedAt: true,
+        expiresAt: true,
+        userUUID: true,
+        User: { omit: { id: true, password: true } },
+      },
+    });
+
+    if (!sessionFromDatabase || !sessionFromDatabase.User)
+      return { status: 'invalid', databaseSession: null, databaseUser: null };
+
+    const databaseSession = sessionSchema.safeParse(sessionFromDatabase);
+
+    // if Zod parsing fails for some reason, return invalid
+    if (!databaseSession.success)
+      return { status: 'invalid', databaseSession: null, databaseUser: null };
+
+    return {
+      status: 'valid',
+      databaseSession: databaseSession.data,
+      databaseUser: sessionFromDatabase.User,
+    };
+  }
+
+  async getUserSessions(userUUID: string): Promise<Session[]> {
+    const sessions = await this.prisma.session.findMany({
+      where: { userUUID },
+    });
+
+    return sessions;
+  }
+
+  /** **Deletes sessions that belongs to a specific user** */
+  async deleteUserSessions(userUUID: string): Promise<void> {
+    await this.prisma.session.deleteMany({ where: { userUUID } });
+
+    return;
+  }
+
+  /** **Deletes ALL the expired sessions no matter who owns it** */
+  async deleteExpiredSessions(): Promise<void> {
+    await this.prisma.session.deleteMany({
+      where: { expiresAt: { lte: new Date() } },
+    });
+
+    return;
+  }
+
+  /** **Updates a specific given session** */
+  async updateSessionExpirationDate(
+    sessionUUID: string,
+    newSessionExpirationDate: Date,
+  ): Promise<void> {
+    await this.prisma.session.update({
+      where: { uuid: sessionUUID },
+      data: { expiresAt: newSessionExpirationDate },
+    });
+
+    return;
+  }
+}
