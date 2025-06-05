74eb7e9db1d169038ce4badc0c1d54c0cd8e70cc
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-05 13:21:36 +0300

    Update utils/handleError.ts

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -1,15 +1,20 @@
 import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
 
 export function handleError(e: unknown) {
   if (e instanceof Error === false) {
     console.error(e);
-    return 'Tuntematon Virhe';
+    toast('Tuntematon virhe');
+    return 'Tuntematon virhe';
   }
 
   if (e instanceof AxiosError) {
     console.error('AxiosError happened', e.response?.data);
-    return 'Axios Error';
+    toast('Lähetetyssä pyynnössä palvelimelle tapahtui virhe ');
+    return 'Lähetetyssä pyynnössä palvelimelle tapahtui virhe ';
   }
 
+  console.error(e);
+  toast('Palvelinvirhe');
   return 'Palvelinvirhe';
 }
