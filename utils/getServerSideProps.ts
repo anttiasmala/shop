a869eeed11892e29132d7976553052c256b51f87
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-06 14:28:31 +0300

    Add getServerSidePropsAdmingOnly function to getServerSideProps.ts

--- utils/getServerSideProps.ts
+++ utils/getServerSideProps.ts
@@ -1,7 +1,16 @@
 import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next';
 import { auth, validateRequest } from '~/backend/auth/auth';
+import { logUserOut } from '~/pages/api/auth/logout';
 import { GetUser, User } from '~/shared/types';
 import { getUserSchema } from '~/shared/zodSchemas';
+
+/**
+ * **>>IMPORTANT<<**
+ *
+ * This function is ran in backend!
+ *
+ * **>>IMPORTANT<<**
+ */
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext,
@@ -38,6 +47,56 @@ export async function getServerSideProps(
       // to convert the object to a plain object (specifically here we convert
       // the user.createdAt Date object to a string).
       user: JSON.parse(JSON.stringify(returnThis.data)) as GetUser,
+    },
+  };
+}
+
+/**
+ * **>>IMPORTANT<<**
+ *
+ * This function is ran in backend!
+ *
+ * **>>IMPORTANT<<**
+ */
+
+export async function getServerSidePropsAdminOnly(
+  context: GetServerSidePropsContext,
+): Promise<GetServerSidePropsResult<{ user: User }>> {
+  const cookieData = await validateRequest(context.req, context.res);
+
+  if (
+    !cookieData.user ||
+    !cookieData.session ||
+    !cookieData.session.isLoggedIn ||
+    cookieData.user.role !== 'ADMIN'
+  ) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/auth/login',
+      },
+    };
+  }
+  const returnThis = getUserSchema.safeParse(cookieData.user);
+  if (returnThis.error) {
+    await logUserOut(cookieData.session.uuid);
+    await auth.deleteSession(cookieData.session.uuid);
+
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/auth/login',
+      },
+    };
+  }
+
+  return {
+    props: {
+      // getStaticProps can only return plain objects (i.e., objects with
+      // primitive values). This is why we use JSON.parse and JSON.stringify
+      // to convert the object to a plain object (specifically here we convert
+      // the user.createdAt Date object to a string).
+      user: JSON.parse(JSON.stringify(returnThis.data)) as User,
     },
   };
 }
