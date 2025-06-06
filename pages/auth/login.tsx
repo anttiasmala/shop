faedcf80982213859abb16bbf45da43bed76de07
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-06 13:22:29 +0300

    Add login page and some code to it. It is placeholder page for now

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,7 +1,138 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { validateRequest } from '~/backend/auth/auth';
+import { Input } from '~/components/Input';
+import { QueryAndMutationKeys, UserLoginDetails } from '~/shared/types';
+import { emailSchema } from '~/shared/zodSchemas';
+import { handleError } from '~/utils/handleError';
+
+const EMPTY_ERRORS = {
+  email: '',
+  password: '',
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const cookieData = await validateRequest(context.req, context.res);
+  if (!cookieData.user || !cookieData.session.isLoggedIn) {
+    return {
+      props: {},
+    };
+  }
+  return {
+    redirect: {
+      permanent: false,
+      destination: '/shop',
+    },
+  };
+}
+
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState(EMPTY_ERRORS);
+
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const { mutateAsync, isPending, error } = useMutation({
+    mutationKey: QueryAndMutationKeys.Login,
+    mutationFn: async (loginCredentials: UserLoginDetails) =>
+      await axios.post('/api/auth/login', loginCredentials),
+
+    onSuccess: () => {
+      queryClient.clear();
+      router.push('/shop').catch((e) => console.error(e));
+    },
+  });
+
+  useEffect(() => {
+    toast(error?.message);
+  }, [error]);
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    try {
+      // clear the errors
+      setErrors(EMPTY_ERRORS);
+      // error found stop the logging process
+      if (checkFields() === true) {
+        return;
+      }
+      await mutateAsync({ email, password });
+    } catch (e) {
+      console.error('Login error:', e);
+      handleError(e);
+    }
+  }
+
+  function checkFields() {
+    let errorFound = false;
+    if (password.length <= 0) {
+      setErrors((prevValue) => ({
+        ...prevValue,
+        password: 'Salasana on pakollinen',
+      }));
+      errorFound = true;
+    }
+
+    const emailParse = emailSchema.safeParse(email);
+    if (emailParse.success === false) {
+      setErrors((prevValue) => ({
+        ...prevValue,
+        email: emailParse.error.errors[0].message,
+      }));
+      errorFound = true;
+    }
+
+    return errorFound;
+  }
+
   return (
-    <div>
-      <p>Hello from login page</p>
-    </div>
+    <main className="h-screen w-full">
+      <div className="flex h-full w-full flex-col items-center">
+        <p className="mb-6 text-center text-2xl font-bold">Login</p>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="mb-2 block font-bold">
+              Email
+            </label>
+            <Input
+              type="email"
+              className="w-full rounded border border-gray-300 p-2"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && (
+              <p className="mt-1 text-sm text-red-500">{errors.email}</p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="mb-2 block font-bold">
+              Password
+            </label>
+            <Input
+              type="password"
+              autoComplete="off"
+              className="w-full rounded border border-gray-300 p-2"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && (
+              <p className="mt-1 text-sm text-red-500">{errors.password}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className={`button-54 w-full rounded bg-yellow-300 p-2 text-white`}
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
