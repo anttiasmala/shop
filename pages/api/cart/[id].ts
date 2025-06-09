e4d6700980c88db2ec22b1094eba45f5339ed655
Antti <129953899+anttiasmala@users.noreply.github.com>
2025-06-09 12:03:05 +0300

    Move handlePOST from /cart/index.ts -> /cart/[id].ts

--- pages/api/cart/[id].ts
+++ pages/api/cart/[id].ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { handleError } from '~/backend/handleError';
+import { HttpError } from '~/backend/HttpError';
 import prisma from '~/prisma';
+import { createCartItemSchema } from '~/shared/zodSchemas';
 
 export default async function Handler(
   req: NextApiRequest,
@@ -8,6 +10,10 @@ export default async function Handler(
 ) {
   try {
     const requestMethod = req.method;
+
+    if (requestMethod === 'POST') {
+      return await handlePOST(req, res);
+    }
 
     if (requestMethod === 'DELETE') {
       return await handleDELETE(req, res);
@@ -17,6 +23,65 @@ export default async function Handler(
   } catch (e) {
     return handleError(res, e);
   }
+}
+
+async function handlePOST(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+  const requestBodyParse = createCartItemSchema.safeParse(req.body);
+  if (requestBodyParse.success === false) {
+    throw new HttpError('Invalid req.body given', 400);
+  }
+  const productToBeAdded = requestBodyParse.data;
+  const product = await prisma.product.findFirstOrThrow({
+    where: {
+      id: requestBodyParse.data.id,
+    },
+  });
+  const cart = await prisma.cart.findFirstOrThrow({
+    where: {
+      id: 1,
+    },
+  });
+
+  const productExists = await prisma.cartItem.findFirst({
+    where: {
+      productUUID: product.uuid,
+    },
+  });
+
+  if (productExists) {
+    const updatedItem = await updateExistingCartItem(
+      productExists.id,
+      productExists.amount,
+      productToBeAdded.amount,
+    );
+
+    return res.status(200).json(updatedItem);
+  }
+
+  const createdItem = await prisma.cartItem.create({
+    data: {
+      cartUUID: cart.uuid,
+      productUUID: product.uuid,
+      amount: productToBeAdded.amount,
+    },
+  });
+  return res.status(200).json(createdItem);
+}
+
+async function updateExistingCartItem(
+  id: number,
+  amountBeforeUpdate: number,
+  amountToBeAdded: number,
+) {
+  await prisma.cartItem.update({
+    where: {
+      id,
+    },
+    data: {
+      amount: amountBeforeUpdate + amountToBeAdded,
+    },
+  });
 }
 
 async function handleDELETE(req: NextApiRequest, res: NextApiResponse) {
