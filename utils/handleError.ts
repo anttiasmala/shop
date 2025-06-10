4b124faf4fc32a8d4042603b48d8325d0252ed77
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-10 14:08:30 +0300

    Add more error handlers to handleError. Errors are more accurate now

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -1,6 +1,14 @@
 import { AxiosError } from 'axios';
 import { toast } from 'react-toastify';
 
+const POSSIBLE_ERRORS = {
+  'unexpected token': 'JSON-muoto on virheellinen',
+  'is not valid json': 'JSON-muoto on virheellinen',
+  'user was not found on the server!': 'Käyttäjää ei ole rekistöröity',
+} as const;
+
+type KnownErrorTexts = keyof typeof POSSIBLE_ERRORS;
+
 export function handleError(e: unknown) {
   if (e instanceof Error === false) {
     console.error(e);
@@ -9,9 +17,32 @@ export function handleError(e: unknown) {
   }
 
   if (e instanceof AxiosError) {
+    const errorText =
+      e.response?.data && typeof e.response.data === 'string'
+        ? POSSIBLE_ERRORS[e.response.data.toLowerCase() as KnownErrorTexts]
+        : null;
+
     console.error('AxiosError happened', e.response?.data);
-    toast('Lähetetyssä pyynnössä palvelimelle tapahtui virhe ');
-    return 'Lähetetyssä pyynnössä palvelimelle tapahtui virhe ';
+    toast(errorText || 'Lähetetyssä pyynnössä palvelimelle tapahtui virhe ');
+    return errorText || 'Lähetetyssä pyynnössä palvelimelle tapahtui virhe ';
+  }
+
+  for (const [key, value] of Object.entries(POSSIBLE_ERRORS)) {
+    if (e.message.toLowerCase().includes(key)) {
+      console.error(e);
+      toast(value);
+      console.error(value);
+      return value;
+    }
+  }
+
+  const knownErrorText =
+    POSSIBLE_ERRORS[e.message.toLowerCase() as KnownErrorTexts];
+
+  if (knownErrorText) {
+    console.error(e);
+    toast(knownErrorText);
+    return knownErrorText;
   }
 
   console.error(e);
