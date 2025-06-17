041062ebff24253d06b525300eda45f8a47e3299
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-06-17 11:07:15 +0300

    Create Main.tsx

---
+++ components/Main.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export function Main({
+  mainClassName,
+  firstDivClassName,
+  secondDivClassName,
+  children,
+}: {
+  mainClassName?: string;
+  firstDivClassName?: string;
+  secondDivClassName?: string;
+  children: ReactNode;
+}) {
+  return (
+    <main className={twMerge('h-screen w-full bg-white', mainClassName)}>
+      <div className={twMerge('flex w-full justify-center', firstDivClassName)}>
+        <div className={twMerge('w-full sm:max-w-1/2', secondDivClassName)}>
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
