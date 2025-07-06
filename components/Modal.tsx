bdb91f05ebaa6347588474bb27425428790285a9
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-07-06 11:18:10 +0300

    Make Modal.tsx to take optional parameters of first and secondDivClassNames

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,29 @@
 import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export function Modal({ children }: { children?: ReactNode }) {
+export function Modal({
+  children,
+  firstDivClassName,
+  secondDivClassName,
+}: {
+  children?: ReactNode;
+  firstDivClassName?: string;
+  secondDivClassName?: string;
+}) {
   return (
     <>
-      <div className="fixed top-0 left-0 z-98 h-full w-full bg-black opacity-80" />
-      <div className="fixed top-[50%] left-[50%] z-99 translate-x-[-50%] translate-y-[-50%]">
+      <div
+        className={twMerge(
+          'fixed top-0 left-0 z-98 h-full w-full bg-black opacity-80',
+          firstDivClassName,
+        )}
+      />
+      <div
+        className={twMerge(
+          'fixed top-[50%] left-[50%] z-99 translate-x-[-50%] translate-y-[-50%]',
+          secondDivClassName,
+        )}
+      >
         {children}
       </div>
     </>
