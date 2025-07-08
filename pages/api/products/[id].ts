2334944527bc57f9c110ccd9da649a76e8edcd30
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-07-08 12:34:12 +0300

    Add /api/products/[id].ts

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { validateRequest } from '~/backend/auth/auth';
+import { HttpError } from '~/backend/HttpError';
+import { checkIsAdminFromValidateRequest } from '~/backend/utils';
+import prisma from '~/prisma';
+import { patchProductSchema } from '~/shared/zodSchemas';
+
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    return await handleGET(req, res);
+  }
+
+  res.status(405).send('GET is valid method');
+}
+
+async function handleGET(req: NextApiRequest, res: NextApiResponse) {
+  const queryId = req.query.id;
+  if (queryId === undefined || typeof queryId !== 'string') {
+    return res.status(400).send('ID is mandatory in query');
+  }
+  const numberQueryId = Number(queryId);
+  if (Number.isNaN(numberQueryId)) {
+    return res.status(400).send('Invalid ID given');
+  }
+
+  const product = await prisma.product.findUnique({
+    where: { id: numberQueryId },
+    omit: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  res.status(200).json(product);
+}
