72725400ee805a6921f7862a8146d2a838c5984b
Antti Asmala <129953899+anttiasmala@users.noreply.github.com>
2025-07-15 15:56:51 +0300

    Change useEffect to useEffectAfterInitialRender for now

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -14,6 +14,7 @@ import { InferGetServerSidePropsType } from 'next';
 import { getServerSidePropsNoLoginRequired as getServerSideProps } from '~/utils/getServerSideProps';
 import { Main } from '~/components/Main';
 import { BASE_IMAGE_URL } from '~/utils/constants';
+import { useEffectAfterInitialRender } from '~/hooks/useEffectAfterInitialRender';
 
 // Does not require login
 export { getServerSideProps };
@@ -32,7 +33,7 @@ export default function Cart({
     retry: false,
   });
 
-  useEffect(() => {
+  useEffectAfterInitialRender(() => {
     const cartUUID = window.localStorage.getItem('cartUUID');
     if (cartUUID) {
       void axios
